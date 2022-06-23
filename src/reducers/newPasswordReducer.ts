fd93f42e94a5013968600cd5b1f096460e597ee1
a.n.darashevich <a.n.darashevich@gmail.com>
2022-06-23 10:44:13 +0300

    add newPasswordReducer

---
+++ src/reducers/newPasswordReducer.ts
@@ -0,0 +1,12 @@
+
+const initialState = ""
+
+export const rewPasswordReducer = (state: any = initialState, action: any): any => {
+    switch (action.type) {
+        case "": {
+            return
+        }
+        default:
+            return state
+    }
+}
