75842337f315b6fe738c221ef5c1a72a3d30662e
a.n.darashevich <a.n.darashevich@gmail.com>
2022-07-13 07:36:27 +0300

    13.07.22 (add learn)

--- src/components/pages/cards/Cards.tsx
+++ src/components/pages/cards/Cards.tsx
@@ -14,6 +14,7 @@ import DeleteIcon from '@mui/icons-material/Delete';
 import UnfoldMoreIcon from '@mui/icons-material/UnfoldMore';
 import ArrowBackIcon from '@mui/icons-material/ArrowBack';
 import EditIcon from '@mui/icons-material/Edit';
+import Button from "@mui/material/Button";
 import {useNavigate, useParams} from "react-router-dom";
 import {useAppDispatch, useAppSelector} from "../../../store/store";
 import {addCardTC, deleteCardTC, getCardsTC, updateCardTC} from "../../../reducers/cardsReduser";
@@ -22,196 +23,199 @@ import styles from "../packs/Packs.module.css";
 import {Paginator} from "../../common/pages/pagination/Paginator";
 import {Selector} from "../../common/pages/select/Selector";
 import {SearchAppBar} from "../searchBar/SearchBar";
-import {Button} from "@mui/material";
+import AddNewCardModal from './modals/AddNewCardModal';
+import {DeleteCardModal} from './modals/DeleteCardModal';
+import {UpdateCardModal} from './modals/UpdateCardModal'
 
 
 export const Cards = React.memo(() => {
-    const dispatch = useAppDispatch()
-    const navigate = useNavigate()
+	const dispatch = useAppDispatch()
+	const navigate = useNavigate()
 
-    const {packid, packname} = useParams()
+	const {packid, packname} = useParams()
 
-    const page = useAppSelector(state => state.cards.page)
-    const pageCount = useAppSelector(state => state.cards.pageCount)
-    const cards = useAppSelector(state => state.cards.cards)
-    const cardsTotalCount = useAppSelector(state => state.cards.cardsTotalCount)
-	const loginUserId = useAppSelector(state=>state.login.loginUserId)
+	const page = useAppSelector(state => state.cards.page)
+	const pageCount = useAppSelector(state => state.cards.pageCount)
+	const cards = useAppSelector(state => state.cards.cards)
 	const user_id = useAppSelector(state => state.profile.userId)
+	const cardsTotalCount = useAppSelector(state => state.cards.cardsTotalCount)
 
-    useEffect(() => {
-        if (packid) {
-            dispatch(getCardsTC({cardsPack_id: packid}))
-        }
-    }, [])
+	useEffect(() => {
+		if (packid) {
+			dispatch(getCardsTC({cardsPack_id: packid}))
+		}
+	}, [])
 
-    const navigateBackHandler = () => {
-        navigate('/packs-page')
-    }
+	const navigateBackHandler = () => {
+		navigate('/packs-page')
+	}
 
-    const valueRating = 4
+	const handlerAddBtn = () => {
+		if (packid) {
+			dispatch(addCardTC({cardsPack_id: packid}))
+		}
+	}
 
-    const handlerAddBtn = () => {
-        if (packid) {
-            dispatch(addCardTC({cardsPack_id: packid}))
-        }
-    }
+	const handlerDeleteCard = (cardId: string) => {
+		if (packid) {
+			dispatch(deleteCardTC(packid, cardId))
+		}
+	}
 
-    const handlerDeleteCard = (cardId: string) => {
-        if (packid) {
-            dispatch(deleteCardTC(packid, cardId))
-        }
-    }
+	const handlerUpdateCard = (cardId: string) => {
+		if (packid) {
+			dispatch(updateCardTC({_id: cardId, question: 'update name card', answer: 'lalala'}, packid))
+		}
+	}
 
-    const handlerUpdateCard = (cardId: string) => {
-        if (packid) {
-            dispatch(updateCardTC({_id: cardId, question: 'update name card'}, packid))
-        }
-    }
+	const [sortUpdate, setSortUpdate] = useState(false)//для сортировки по update
 
-    const [sortUpdate, setSortUpdate] = useState(false)//для сортировки по update
+	const handleSortByUpdate = () => {
+		let sortUpdateStr
+		sortUpdate ? sortUpdateStr = '0updated' : sortUpdateStr = '1updated'
+		if (packid) {
+			dispatch(getCardsTC({
+				cardsPack_id: packid,
+				sortCards: sortUpdateStr,
+				page,
+				pageCount
+			}))
+		}
+		setSortUpdate(!sortUpdate)
+	}
 
-    const handleSortByUpdate = () => {
-        let sortUpdateStr
-        sortUpdate ? sortUpdateStr = '0updated' : sortUpdateStr = '1updated'
-        if (packid) {
-            dispatch(getCardsTC({
-                cardsPack_id: packid,
-                sortCards: sortUpdateStr,
-                page,
-                pageCount
-            }))
-        }
-        setSortUpdate(!sortUpdate)
-    }
+	const onPageChange = (page: number) => {
+		if (packid) {
+			dispatch(getCardsTC({
+				cardsPack_id: packid,
+				page,
+				pageCount,
+				sortCards: `${Number(sortUpdate)}updated`,
+			}))
+		}
+	}
 
-    const onPageChange = (page: number) => {
-        if (packid) {
-            dispatch(getCardsTC({
-                cardsPack_id: packid,
-                page,
-                pageCount,
-                sortCards: `${Number(sortUpdate)}updated`,
-            }))
-        }
-    }
+	const onChangePageCount = (pageCount: number) => {
+		if (packid) {
+			dispatch(getCardsTC({
+				cardsPack_id: packid,
+				page,
+				pageCount,
+				sortCards: `${Number(sortUpdate)}updated`,
+			}))
+		}
+	}
 
-    const onChangePageCount = (pageCount: number) => {
-        if (packid) {
-            dispatch(getCardsTC({
-                cardsPack_id: packid,
-                page,
-                pageCount,
-                sortCards: `${Number(sortUpdate)}updated`,
-            }))
-        }
-    }
+	const onSearchPacks = (cardQuestion: string) => {
+		if (packid) {
+			dispatch(getCardsTC({
+				cardsPack_id: packid,
+				cardQuestion,
+				page,
+				pageCount,
+				sortCards: `${Number(sortUpdate)}updated`,
+			}))
+		}
+	}
 
-    const onSearchPacks = (cardQuestion: string) => {
-        if (packid) {
-            dispatch(getCardsTC({
-                cardsPack_id: packid,
-                cardQuestion,
-                page,
-                pageCount,
-                sortCards: `${Number(sortUpdate)}updated`,
-            }))
-        }
-    }
+	const deleteCardHandler = (cardId: string) => {
+		if (packid) {
+			dispatch(deleteCardTC(packid, cardId))
+		}
+	}
+	return (
+		<>
+			<Navbar/>
+			<div className={style.container}>
 
-    return (
-        <>
-            <Navbar/>
-            <div className={style.container}>
+				<div className={style.back}>
+					<ArrowBackIcon onClick={navigateBackHandler} className={style.arrowBackIcon}/>
+					<p className={style.packName}>{packname}</p>
+				</div>
 
-                <div className={style.back}>
-                    <ArrowBackIcon onClick={navigateBackHandler} className={style.arrowBackIcon}/>
-                    <p>{packname}</p>
-                </div>
+				<TableContainer className={style.tableContainer} elevation={3} component={Paper}>
 
-                <TableContainer className={style.tableContainer} elevation={3} component={Paper}>
+					<SearchAppBar onSearchPacks={onSearchPacks}
+					              children={<AddNewCardModal/>}
+					/>
 
-                    <SearchAppBar onSearchPacks={onSearchPacks}
-                                  children={
-                                      <Button variant="contained" color="primary"
-                                              onClick={handlerAddBtn}>
-                                          Add Card
-                                      </Button>}
-                    />
+					<Table aria-label="simple table">
 
-                    <Table aria-label="simple table">
+						<TableHead>
+							<TableRow>
+								<TableCell sx={{'fontWeight': 'bold'}}>Question</TableCell>
+								<TableCell sx={{'fontWeight': 'bold'}} align="right">Answer</TableCell>
 
-                        <TableHead>
-                            <TableRow>
-                                <TableCell sx={{'font-weight': 'bold'}}>Question</TableCell>
-                                <TableCell sx={{'font-weight': 'bold'}} align="right">Answer</TableCell>
+								<TableCell className={style.updated} sx={{'fontWeight': 'bold'}} align={'right'}>
+									<div className={styles.headerSortText}><b>Updated</b>
+										<div>
+											<IconButton onClick={handleSortByUpdate} aria-label="arrow" size="small">
+												<UnfoldMoreIcon fontSize="inherit"/>
+											</IconButton>
+										</div>
+									</div>
+								</TableCell>
 
-                                <TableCell className={style.updated} sx={{'font-weight': 'bold'}} align={'right'}>
-                                    <div className={styles.headerSortText}><b>Updated</b>
-                                        <div>
-                                            <IconButton onClick={handleSortByUpdate} aria-label="arrow" size="small">
-                                                <UnfoldMoreIcon fontSize="inherit"/>
-                                            </IconButton>
-                                        </div>
-                                    </div>
-                                </TableCell>
+								<TableCell sx={{'fontWeight': 'bold'}} align="right">Grade</TableCell>
+								<TableCell sx={{'fontWeight': 'bold'}} align="right">Actions</TableCell>
+							</TableRow>
+						</TableHead>
+						<TableBody>
 
-                                <TableCell sx={{'font-weight': 'bold'}} align="right">Grade</TableCell>
-                                <TableCell sx={{'font-weight': 'bold'}} align="right">Actions</TableCell>
-                            </TableRow>
-                        </TableHead>
-                        <TableBody>
+							{cards.map((card) => (
+								<TableRow
+									key={card._id}
+									sx={{'&:last-child td, &:last-child th': {border: 0}}}
+								>
+									<TableCell component="th" scope="row">{card.question}</TableCell>
+									<TableCell align="right">{card.answer}</TableCell>
+									<TableCell align="right">{
+										`${(card.updated).slice(8, 10)}.${(card.updated).slice(5, 7)}.${(card.updated).slice(0, 4)}`
+									}</TableCell>
 
-                            {cards.map((card) => (
-                                <TableRow
-                                    key={card._id}
-                                    sx={{'&:last-child td, &:last-child th': {border: 0}}}
-                                >
-                                    <TableCell component="th" scope="row">{card.question}</TableCell>
-                                    <TableCell align="right">{card.answer}</TableCell>
-                                    <TableCell align="right">{
-                                        `${(card.updated).slice(8, 10)}.${(card.updated).slice(5, 7)}.${(card.updated).slice(0, 4)}`
-                                    }</TableCell>
+									<TableCell align="right">
+										<Rating name="read-only"
+										        precision={0.5}
+										        value={card.grade}
+										        size={"small"} readOnly/>
+									</TableCell>
 
-                                    <TableCell align="right">
-                                        <Rating name="read-only" value={valueRating} size={"small"} readOnly/>
-                                    </TableCell>
+									<TableCell align="right">
+										{(card.user_id === user_id)
+											? <div className={style.iconBlock}>
 
-                                    <TableCell align="right">
-                                        {(card.user_id === user_id || card.user_id === loginUserId)
-                                            ? <><IconButton onClick={() => handlerDeleteCard(card._id)}
-                                                            aria-label="delete" size="small">
-                                                <DeleteIcon fontSize="inherit"/>
-                                            </IconButton>
+													<DeleteCardModal handleOperation={() => deleteCardHandler(card._id)}
+													                 cardName={card.question}/>
 
-                                                <IconButton onClick={() => handlerUpdateCard(card._id)}
-                                                            aria-label="edit" size="small">
-                                                    <EditIcon fontSize="inherit"/>
-                                                </IconButton></>
+													<UpdateCardModal packid={packid!}
+													                 question={card.question}
+													                 answer={card.answer}
+													                 cardId={card._id}/>
+											</div>
+											: null
+										}
+									</TableCell>
 
-                                            : null
-                                        }
-                                    </TableCell>
+								</TableRow>
+							))}
+						</TableBody>
 
-                                </TableRow>
-                            ))}
-                        </TableBody>
+					</Table>
+				</TableContainer>
 
-                    </Table>
-                </TableContainer>
+				<div className={styles.paginatorBlock}>
+					<div className={styles.paginator}>
+						<Paginator onPageChange={onPageChange} totalCount={cardsTotalCount} pageCount={pageCount}/>
+					</div>
+					<div className={styles.selector}>
+						Show
+						<Selector value={pageCount} onChangePageCount={onChangePageCount}/>
+						Packs per page
+					</div>
+				</div>
 
-                <div className={styles.paginatorBlock}>
-                    <div className={styles.paginator}>
-                        <Paginator onPageChange={onPageChange} pageCount={pageCount} totalCount={cardsTotalCount}/>
-                    </div>
-                    <div className={styles.selector}>
-                        Show
-                        <Selector value={pageCount} onChangePageCount={onChangePageCount}/>
-                        Packs per page
-                    </div>
-                </div>
-
-            </div>
-        </>
-    )
+			</div>
+		</>
+	)
 })
 
